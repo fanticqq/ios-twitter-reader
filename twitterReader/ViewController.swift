61f542f8dbab57cf48d57799dde564137404380f
Admin <admin@mac-admin.local>
2016-07-19 22:47:46 +0300

    Initial Commit

---
+++ twitterReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterReader
+//
+//  Created by Admin on 19.07.16.
+//  Copyright © 2016 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
